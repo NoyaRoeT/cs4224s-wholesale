22b925ef0762f61a894cf674cf4cd043101df801
Leaftyyyy <rayonteo@gmail.com>
2024-10-08 16:48:58 +0800

    Fix synchronization issue between coordinator and workers

--- scripts/task.sh
+++ scripts/task.sh
@@ -6,6 +6,7 @@ REMAINDER=$(($SLURM_PROCID % 5))
 SIGNAL_DIR="$HOME/$SLURM_JOB_ID"
 READY_FILE="$SIGNAL_DIR/${HOSTNAME}_ready"
 DONE_FILE="$SIGNAL_DIR/done_$SLURM_PROCID"
+EXIT_FILE="$SIGNAL_DIR/exit_$HOSTNAME"
 
 NUM_CLIENTS=20
 CLIENT_TASKS=(1 2 3 4 6 7 8 9 11 12 13 14 16 17 18 19 21 22 23 24)
@@ -59,6 +60,29 @@ wait_clients_done() {
     done
 }
 
+signal_worker_exit() {
+	touch $EXIT_FILE
+}
+
+wait_workers_exit() {
+	local all_exit=0
+	while [ $all_exit -eq 0 ]; do
+        local exit_count=0
+        for i in {0..4}; do
+            local node_name="xcne$i"
+            if [ -f "$SIGNAL_DIR/exit_$node_name" ]; then
+                exit_count=$((exit_count + 1))
+            fi
+        done
+
+        if [ $exit_count -eq 5 ]; then
+            all_exit=1
+        else
+			sleep 1 # Sleep to avoid busy waiting
+		fi
+    done
+}
+
 if [ ${REMAINDER} -eq 0 ]; then
 	mkdir -p $SIGNAL_DIR # prepare for file-based synchronization
 
@@ -92,6 +116,7 @@ if [ ${REMAINDER} -eq 0 ]; then
 	wait_clients_done
 
 	if [ "${HOSTNAME}" = "$coordinator_node" ]; then
+		wait_workers_exit
 		echo "Performing clean up..."
 		clean_up
 	fi

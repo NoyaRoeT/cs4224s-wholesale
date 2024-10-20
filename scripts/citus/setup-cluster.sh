8c222021d31f1954b7217746418b4a97f37c0691
NoyaReoT <rayonteo@gmail.com>
2024-10-20 19:40:06 +0800

    Fix error adding worker nodes

--- scripts/citus/setup-cluster.sh
+++ scripts/citus/setup-cluster.sh
@@ -2,7 +2,6 @@
 coordinator_node=$1
 HOSTNAME=$(hostname)
 REMAINDER=$(($SLURM_PROCID % 5))
-HOSTNAMES=()
 
 # Steps to execute on all nodes
 if [ ${REMAINDER} -eq 0 ]; then
@@ -10,13 +9,6 @@ if [ ${REMAINDER} -eq 0 ]; then
     $HOME/pgsql/bin/pg_ctl -D $PGDATA -l logfile start
     createdb
     psql -p $PGPORT -c "CREATE EXTENSION citus;"
-
-    if [ "${HOSTNAME}" != "$coordinator_node" ]; then
-        HOSTNAMES+=("$HOSTNAME")
-    fi
-    
-else
-    true
 fi
 
 sleep 10
@@ -24,12 +16,15 @@ sleep 10
 # Steps to execute on coordinator node
 if [ ${REMAINDER} -eq 0 ] && [ "${HOSTNAME}" = "$coordinator_node" ]; then
     psql -c "SELECT * FROM citus_set_coordinator_host('$coordinator_node', $PGPORT);"
-    for HOSTNAME in "${HOSTNAMES[@]}"; do
-        psql -c "SELECT * FROM citus_add_node('$HOSTNAME', $PGPORT);"
+
+    allocated_nodes=($(scontrol show hostnames $SLURM_NODELIST))
+
+    for node in "${allocated_nodes[@]}"; do
+        if [ "$node" != "$coordinator_node" ]; then
+            psql -c "SELECT * FROM citus_add_node('$node', $PGPORT);"
+        fi
     done
-else
-    true
 fi
 
-sleep 5
+sleep 10
 

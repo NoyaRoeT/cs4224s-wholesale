6fcc7fab09523a5efbac6f60b87a15c41bf5e265
NoyaReoT <rayonteo@gmail.com>
2024-10-22 12:07:06 +0800

    Prepare task.sh for output scripts

--- scripts/task.sh
+++ scripts/task.sh
@@ -8,6 +8,8 @@ READY_FILE="$SIGNAL_DIR/${HOSTNAME}_ready"
 DONE_FILE="$SIGNAL_DIR/done_$SLURM_PROCID"
 EXIT_FILE="$SIGNAL_DIR/exit_$HOSTNAME"
 
+OUTPUT_DIR="../output"
+
 NUM_CLIENTS=20
 CLIENT_TASKS=(1 2 3 4 6 7 8 9 11 12 13 14 16 17 18 19 21 22 23 24)
 
@@ -34,7 +36,7 @@ wait_db_ready() {
         if [ $ready_count -eq 5 ]; then
             all_ready=1
         else
-			sleep 1 # Sleep to avoid busy waiting
+			sleep 1
 		fi
     done
 }
@@ -56,41 +58,30 @@ wait_clients_done() {
         if [ $done_count -eq $NUM_CLIENTS ]; then
             all_done=1
         else
-            sleep 1  # Sleep to avoid busy waiting
+            sleep 1
         fi
     done
 }
 
-signal_worker_exit() {
-	touch $EXIT_FILE
+signal_output_done() {
+    touch "$SIGNAL_DIR/OUTPUT"
 }
 
-wait_workers_exit() {
-	local all_exit=0
-	while [ $all_exit -eq 0 ]; do
-        local exit_count=0
-        for node_name in "${NODES[@]}"; do
-            if [ -f "$SIGNAL_DIR/exit_$node_name" ]; then
-                exit_count=$((exit_count + 1))
-            fi
-        done
-
-        if [ $exit_count -eq 5 ]; then
-            all_exit=1
-        else
-			sleep 1 # Sleep to avoid busy waiting
-		fi
+wait_output_done() {
+    while [ ! -f "$SIGNAL_DIR/OUTPUT" ]; do
+        sleep 1
     done
 }
 
 if [ ${REMAINDER} -eq 0 ]; then
 	mkdir -p $SIGNAL_DIR # prepare for file-based synchronization
+    mkdir -p $OUTPUT_DIR # prepare output dir if it doesn't exist
 
 	# Start db servers
 	$HOME/pgsql/bin/pg_ctl -D $PGDATA -l logfile restart
 	if [ "${HOSTNAME}" = "$coordinator_node" ]; then
 		source "$HOME/${PGUSER}_venv/bin/activate"
-		echo "Loading test data..."
+		echo "Loading data"
         python ../python/table_creation.py
         python ../python/data_ingestion.py --w "../data_files/warehouse.csv" --d "../data_files/district.csv" --c "../data_files/customer.csv" --o "../data_files/order.csv" --i "../data_files/item.csv" --ol "../data_files/order-line.csv" --s "../data_files/stock.csv"
 	fi
@@ -109,14 +100,16 @@ if [ ${REMAINDER} -ne 0 ]; then
 fi
 
 if [ ${REMAINDER} -eq 0 ]; then
-	wait_clients_done
-	signal_worker_exit
 	if [ "${HOSTNAME}" = "$coordinator_node" ]; then
-		wait_workers_exit
+        wait_clients_done
+        # Run output scripts
+        signal_output_done
+
 		echo "Performing clean up..."
 		clean_up
+    else
+        wait_output_done
 	fi
-
 	# stop db servers
 	$HOME/pgsql/bin/pg_ctl -D $PGDATA stop
 fi
